b16e747dbd78759665121885fd2b56195e82b0f8
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-23 15:08:13 +0200

    starting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'clean db'
+# Orders.destroy_all
+# Van.destroy_all
+# User.destroy_all
+puts 'create seed db'
