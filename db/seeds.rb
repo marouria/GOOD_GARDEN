6475b99ceab4a5b5e062c0cfa81945f2553fbfd8
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-23 15:42:23 +0200

    Plants seeds done with CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
 
 puts 'clean db'
-# Orders.destroy_all
+Plant.destroy_all
 # Van.destroy_all
 # User.destroy_all
 puts 'create seed db'
+
+
+
+csv_plants = File.read(Rails.root.join('db', 'Seeds Good Garden - PLANTS (1).csv'))
+csv = CSV.parse(csv_plants, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Plant.new
+  t.name = row['name']
+  t.sunshine_rate = row ['sunshine_rate']
+  t.watering = row ['watering']
+  t.img_url = "https://source.unsplash.com/#{row ['img_url']}/1000x1000"
+  t.season = row ['season']
+  t.user_level = row ['user_level']
+  t.price = row ['price']
+  t.stock = row ['stock']
+  t.save
+  puts "#{t.name} saved !"
+end
+puts "There are now #{Plant.count} rows in the plants table"
+
+
