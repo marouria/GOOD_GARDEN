1ffcb1a545a1fe779aff41b63088fef16665aae2
Josephine Herlemont <herlemont.josephine@gmail.com>
2021-06-28 17:00:57 +0200

    wip

--- app/javascript/components/conseil.js
+++ app/javascript/components/conseil.js
@@ -7,13 +7,12 @@ const displayPlant = () => {
   const specificAdvise = document.getElementById("specific-advise");
   
   plants.forEach(plant => {
-    plant.addEventListener("click", (event) => {
+    plant.addEventListener("mouseover", (event) => {
       generalAdvise.style.display = "none";
       plantName.innerHTML = event.currentTarget.dataset.plant;
       plantWatering.innerHTML = `Arrosage : ${event.currentTarget.dataset.watering}<i class="fas fa-tint"></i>`;
       plantSunshine.innerHTML = `Exposition : ${event.currentTarget.dataset.sunshine}<i class="fas fa-sun"></i>`;
       specificAdvise.style.display = "block";
-      console.log(event.currentTarget.dataset.plant);
     });
   });
 };
