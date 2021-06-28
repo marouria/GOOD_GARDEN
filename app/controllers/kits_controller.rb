0ad9f5d980da433788d08d4dbe5a25d790abb416
Marie Leuliette <marie.leuliette@gmail.com>
2021-06-28 15:52:12 +0200

    looking for price kit

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -2,7 +2,7 @@ class KitsController < ApplicationController
   def show
     @kit = Kit.find(params[:id])
     @plants = KitPlant.where(kit_id: params[:id])
-    @total_price = 0
+    # @total_price = 0
   end
 
   def new
@@ -14,7 +14,7 @@ class KitsController < ApplicationController
   def create
     @kit = Kit.new
     @kit.user_id = current_user.id
-    @kit.save
+    @kit.save!
     @plants = params[:plant]
     @plants.each do |plant|
       @kitplant = KitPlant.new
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   def create
     @kit = Kit.find(params[:kit_id])
     @order = Order.create!(kit: @kit, amount: @kit.price, status:'pending', user: current_user)
-
     session = Stripe::Checkout::Session.create(
     payment_method_types: ['card'],
     line_items: [{
@@ -11,8 +10,8 @@ class OrdersController < ApplicationController
       currency: 'eur',
       quantity: 1
     }],
-    success_url: order_path(@order),
-    cancel_url: order_path(@order)
+    success_url: order_url(@order),
+    cancel_url: order_url(@order)
   )
 
     @order.update(checkout_session_id: session.id)
