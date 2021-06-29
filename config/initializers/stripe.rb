bc4a7e04b6beb24167e7d79a1c6af9121ab451f7
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-29 09:15:58 +0200

    Stripe test

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
     Rails.configuration.stripe = {
-:publishable_key => ENV["pk_test_KEY"],
-:secret_key      => ENV["sk_test_KEY"]
+:publishable_key => ENV["pk_test_TYooMQauvdEDq54NiTphI7jx"],
+:secret_key      => ENV["sk_test_4eC39HqLyjWDarjtT1zdp7dc"]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
