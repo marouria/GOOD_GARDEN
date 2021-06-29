3e7d310a1a657544222928f5a2fe597ce27e72f2
Marie Leuliette <marie.leuliette@gmail.com>
2021-06-29 19:38:03 +0200

    change kit price into order price

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
     payment_method_types: ['card'],
     line_items: [{
       name: @kit.id,
-      amount: @kit.price_cents,
+      amount: @order.amount_cents,
       currency: 'eur',
       quantity: 1
     }],
