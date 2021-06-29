2650c39c5f31d8496cc3f2a6cf43c75d2316d36f
Josephine Herlemont <herlemont.josephine@gmail.com>
2021-06-29 12:27:38 +0200

    toggle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,14 +30,13 @@ import "bootstrap";
 import {scroll} from '../components/preferences_form';
 import {pointImages} from '../components/preferences_form';
 import {initSweetalert} from '../components/sweetalert';
-import {displayPlant, backToDefault} from '../components/conseil';
+import {displayPlant} from '../components/conseil';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   scroll();
   displayPlant();
-  backToDefault();
   pointImages();
   initSweetalert('#payment-confirmation', {
     title: "Paiement confirmé!",
