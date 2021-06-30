def4bb8340523e6ccb251768b2b3f3267c6e0db5
Marie Leuliette <marie.leuliette@gmail.com>
2021-06-30 21:05:23 +0200

    remove material references from kits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_184240) do
+ActiveRecord::Schema.define(version: 2021_06_30_190411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,6 @@ ActiveRecord::Schema.define(version: 2021_06_30_184240) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
-    t.bigint "material_id"
-    t.index ["material_id"], name: "index_kits_on_material_id"
     t.index ["user_id"], name: "index_kits_on_user_id"
   end
 
@@ -109,7 +107,6 @@ ActiveRecord::Schema.define(version: 2021_06_30_184240) do
   add_foreign_key "kit_materials", "materials"
   add_foreign_key "kit_plants", "kits"
   add_foreign_key "kit_plants", "plants"
-  add_foreign_key "kits", "materials"
   add_foreign_key "kits", "users"
   add_foreign_key "orders", "kits"
   add_foreign_key "orders", "users"
