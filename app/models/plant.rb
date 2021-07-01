ea8b56b56d05cc4fac9c92ae96c5aa4ba3ce7e7b
molki-supreme <marie-chn@hotmail.fr>
2021-07-01 16:53:00 +0200

    search bar

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,4 +2,12 @@ class Plant < ApplicationRecord
   has_many :kit_plants
   has_many :kits, through: :kit_plants
   monetize :price_cents
+
+    include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
 end
