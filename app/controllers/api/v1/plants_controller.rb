24f24b4d03cd01c5e6eac889bcbad1fdf2146dfa
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-11 21:20:10 +0200

    show returned in json

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::PlantsController < Api::V1::BaseController
+  before_action :set_plant, only: [ :show ]
   def index
     @plants = policy_scope(Plant)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
+    authorize @plant  # For Pundit
   end
 end
---
+++ app/views/api/v1/plants/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @plant, :id, :name, :sunshine_rate, :watering, :img_url, :season, :user_level, :price_cents, :description, :price
